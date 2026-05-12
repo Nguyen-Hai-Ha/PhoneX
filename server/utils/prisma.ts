dd746b3a0f6862229f7bc82f943a5628eceae0ee
Putu Wibowo <hanguyen032325@gmail.com>
2026-05-12 15:57:26 +0700

    big update

--- server/utils/prisma.ts
+++ server/utils/prisma.ts
@@ -3,7 +3,13 @@ import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-// Khởi tạo Singleton pattern
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+export const prisma = globalForPrisma.prisma || new PrismaClient({
+  datasources: {
+    db: {
+      // Dùng useRuntimeConfig để lấy databaseUrl đã định nghĩa trong nuxt.config.ts
+      url: useRuntimeConfig().databaseUrl 
+    }
+  }
+})
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
